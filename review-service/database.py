35787254865f0e30649b65535c65eb4bfc8562e9
gemaSyaputra <gemasyaputra04@gmail.com>
2026-01-09 15:06:30 +0700

    first commit

---
+++ review-service/database.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import os
+import time
+
+MONGO_URI= os.getenv("MONGO_URI", "mongodb://admin:admin123@mongo:27017/")
+client = MongoClient(MONGO_URI)
+
+def connect_with_retry():
+    retries = 5
+    while retries:
+        try:
+            client = MongoClient(MONGO_URI)
+            client.admin.command('ping')
+            print("Connected to MongoDB")
+            return client
+        except Exception as e:
+            print(f"MongoDB connection failed: {e}")
+            retries -= 1
+            time.sleep(5)
+    raise Exception("Could not connect to MongoDB after several attempts")
+
+client = connect_with_retry()
+db = client[os.getenv("MONGO_DB_NAME", "reviewdb")] 
+reviews_collection = db["reviews"]
